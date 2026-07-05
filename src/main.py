8d491fa76f060c07c41b2a5f418f1bd82df30aa5
fm25abm <fm25abm@herts.ac.uk>
2026-07-05 20:47:50 +0100

    Implemented OSV scanner wrapper with error handling

--- src/main.py
+++ src/main.py
@@ -1,5 +1,26 @@
+"""
+This script intializes the Patch Prioritisation Engine.
+
+It calls all the individual modules in the correct order. 
+
+Currently, it invokes the scanner module to analyse a project and generate the vulnerability
+report.
+
+Future versions will also call the parser, EPSS, KEV and prioritisation modules.
+"""
+
+from scanner import run_scan
+
 def main():
-    print("Patch Prioritisation Engine")
+
+    project_path = "test-projects/python/flask-demo"
+    output_file = "output/results.json"
+
+    success = run_scan(project_path, output_file)
+
+    if success:
+        print("Results saved to:", output_file)
+
 
 if __name__ == "__main__":
     main()
--- src/scanner.py
+++ src/scanner.py
@@ -1,3 +1,33 @@
-def run_scan():
-    """Run Google OSV-Scanner."""
-    pass
+"""
+This script runs Google OSV-Scanner on the specified project and saves
+the results as a JSON file.
+
+Parameters:
+    project_path (str): Path to the project to scan.
+    output_file (str): Path where the JSON output will be saved.
+
+Returns:
+    bool: True if the scan completed successfully, otherwise False.
+"""
+import subprocess
+
+def run_scan(project_path, output_file):
+
+    command = [
+        "osv-scanner",
+        "scan",
+        project_path,
+        "--format",
+        "json"
+    ]
+
+    try:
+        with open(output_file, "w") as file:
+            subprocess.run(command, stdout=file)
+
+        print(f"Scan completed successfully.")
+        return True
+
+    except Exception as error:
+        print(f"An unexpected error occurred: {error}")
+        return False
