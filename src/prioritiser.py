27c1082cc2f16175c5eaef7cf8be2f15bc8db7a7
fm25abm <fm25abm@herts.ac.uk>
2026-07-15 18:44:42 +0100

    Prioritiser Test

--- src/prioritiser.py
+++ src/prioritiser.py
@@ -1,5 +1,33 @@
+"""
+This script assigns priority to vulnerabilties found by the OSV scanner.
+
+The priority is based on two factors:
+- The EPSS score
+- Whether it is actively exploited; through KEV
+
+Returns:
+    str: Priority Level
+"""
+
 def assign_priority(vulnerability):
-    Critical
-    High
-    Medium
-    Low
+
+    if vulnerability["kev"]:
+        return "Critical"
+    
+    if vulnerability["epss"] >= 0.70:
+        return "High"
+
+    if vulnerability["epss"] >= 0.30:
+        return "Medium"
+    
+    else:
+        return "Low"
+
+if __name__ == "__main__":
+
+    vulnerability = {
+        "epss": 0.82,
+        "kev": False
+    }
+
+    print(assign_priority(vulnerability))
